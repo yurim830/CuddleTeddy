79dd6705c4efb9f2765d6961802c9f4c392a9f40
Yurim Kim <yurim830@gmail.com>
2025-02-18 21:00:43 +0900

    [Chore] 이미지 익스텐션 추가 (#1)

---
+++ AnxietySmasher.swiftpm/Extensions/Image+.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  AnxietySmasher
+//
+//  Created by 김유림 on 2/18/25.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    static var body1: Image {
+        return Image("body1")
+    }
+    
+    static var body2: Image {
+        return Image("body2")
+    }
+    
+    static var faceHappy: Image {
+        return Image("face-happy")
+    }
+    
+    static var faceAww: Image {
+        return Image("face-aww")
+    }
+    
+    static var faceSmileyCry: Image {
+        return Image("face-smiley-cry")
+    }
+    
+    static var faceLove: Image {
+        return Image("face-love")
+    }
+    
+    static var faceEww: Image {
+        return Image("face-eww")
+    }
+    
+    static var faceAngry: Image {
+        return Image("face-angry")
+    }
+    
+    static var faceShocked: Image {
+        return Image("face-shocked")
+    }
+    
+    static var faceSmile: Image {
+        return Image("face-smile")
+    }
+    
+}
