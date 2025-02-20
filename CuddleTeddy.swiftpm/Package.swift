a561fda95ec302f552e999142dd3d903bf3ffd90
Yurim Kim <yurim830@gmail.com>
2025-02-21 08:25:31 +0900

    [Settings] App Icon

--- CuddleTeddy.swiftpm/Package.swift
+++ CuddleTeddy.swiftpm/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "L995XJC5CV",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .cloud),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.orange),
             supportedDeviceFamilies: [
                 .pad,
